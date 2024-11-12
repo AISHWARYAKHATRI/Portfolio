2e74bace2dc8ff669b13e77e367e7fc06e6f3048
AISHWARYAKHATRI <aishwaryakhatri20@gmail.com>
2024-11-13 00:24:31 +0530

    Section 1 enlarged title

--- src/app/components/ParticlesComponent.tsx
+++ src/app/components/ParticlesComponent.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useEffect } from "react";
 import "particles.js";
-import Typewriter from "typewriter-effect";
 
 // Define the type for particlesJS function
 type ParticlesJSParams = {
@@ -240,23 +239,11 @@ const ParticlesComponent: React.FC = () => {
   return (
     <div id="particles-js" className="w-full h-screen">
       <div className="absolute text-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <div className="flex">
-          <div className="flex flex-col">
-            <h2 className="lg:text-[48px] text-4xl font-extrabold leading-12">
-              Hi, I am
-            </h2>
-            <div className="lg:text-[48px] text-4xl my-5 font-extrabold">
-              <div className="bg-gradient-to-r from-pink-500 to-amber-500 text-transparent bg-clip-text py-4">
-                <Typewriter
-                  options={{
-                    strings: ["Aishwarya Khatri", "A Frontend Engineer"],
-                    autoStart: true,
-                    loop: true,
-                  }}
-                />
-              </div>
-            </div>
-          </div>
+        <div className="text-5xl md:text-6xl my-5 font-extrabold xl:text-nowrap">
+          Hi, I&apos;m&nbsp;
+          <span className="bg-gradient-to-r from-pink-500 to-amber-500 text-transparent bg-clip-text py-4">
+            Aishwarya Khatri
+          </span>
         </div>
       </div>
     </div>
